be3a4803b51dff7f872c4451d124957c0d411a87
shopia-sal <shopiasalma599@gmail.com>
2025-10-29 22:06:25 +0700

    Fix Navbar import case sensitivity

--- frontend/src/pages/Menu/Menu.jsx
+++ frontend/src/pages/Menu/Menu.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../../components/Navbar/navbar'
+import Navbar from '../../components/Navbar/Navbar'
 import Footer from '../../components/Footer/Footer'
 import OurMenu from '../../components/OurMenu/OurMenu'
 
