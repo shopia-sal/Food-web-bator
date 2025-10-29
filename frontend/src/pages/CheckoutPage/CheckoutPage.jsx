83b84438b5353a95cad654743d2a1ec7495999d0
shopia-sal <shopiasalma599@gmail.com>
2025-10-29 22:05:40 +0700

    Fix Navbar import case sensitivity.

--- frontend/src/pages/CheckoutPage/CheckoutPage.jsx
+++ frontend/src/pages/CheckoutPage/CheckoutPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Checkout from '../../components/Checkout/Checkout'
 import Footer from '../../components/Footer/Footer'
-import Navbar from '../../components/Navbar/navbar'
+import Navbar from '../../components/Navbar/Navbar'
 
 const CheckoutPage = () => {
   return (
