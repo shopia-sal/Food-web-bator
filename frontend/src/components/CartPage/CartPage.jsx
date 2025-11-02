b30e4bd21984cc50b97caaff7207b0f3a5611112
shopia-sal <shopiasalma599@gmail.com>
2025-11-02 13:36:15 +0700

    .

--- frontend/src/components/CartPage/CartPage.jsx
+++ frontend/src/components/CartPage/CartPage.jsx
@@ -3,7 +3,7 @@ import { useCart } from '../../CartContext/CartContext'
 import { Link } from 'react-router-dom';
 import { FaMinus, FaPlus, FaTimes, FaTrash } from 'react-icons/fa';
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 const CartPage = () => {
 
