7d63307efd458690eabc18282b1cb7cc38bf2b96
shopia-sal <shopiasalma599@gmail.com>
2025-11-02 13:34:10 +0700

    .

--- frontend/src/Login/Login.jsx
+++ frontend/src/Login/Login.jsx
@@ -18,11 +18,10 @@ const Login = ({ onLoginSuccess, onClose }) => {
   const handleSubmit = async e => {
     e.preventDefault();
     try {
-const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/user/login`, {
-  email: formData.email,
-  password: formData.password,
-});
-
+      const res = await axios.post(`${import.meta.env.VITE_API_URL}/api/user/login`, {
+        email: formData.email,
+        password: formData.password,
+      });
 
       console.log('Axios Res:', res);
 
